005a3741064747bfee28d90bf826143fa98602d8
Joseph Schiarizzi <9449596+cupOJoseph@users.noreply.github.com>
2023-05-09 22:40:19 -0400

    Update PuzzleNFT.sol

--- contracts/PuzzleNFT.sol
+++ contracts/PuzzleNFT.sol
@@ -1,35 +1,45 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
 
-contract PuzzleNFT is ERC1155 {
-    uint256 public constant Controller = 1;
-    mapping(uint => mapping(address => bool)) public minted;
+/// @custom:security-contact team@usekeyp.com
+contract PuzzleNFT is ERC1155, Ownable, ERC1155URIStorage{
+    mapping (uint  id => string uri) someMapping;
+    address public minter;
 
-    constructor() ERC1155("https://ipfs.io/ipfs/bafybeihjjkwdrxxjnuwevlqtqmh3iegcadc32sio4wmo7bv2gbf34qs34a/{id}.json") {
-        _mint(msg.sender, Controller, 1, "0xafa9f9f984e47d883db2d017d7ad589007c6a3e8");
+    constructor() ERC1155("") {
+      minter = msg.sender;
     }
 
-    function mint(bytes calldata adminSig, bytes memory data) external {
-      require(_isValid(adminSig), "invalid signature");
-      // require(alreadyMinted(_msgSender()), "already minted");
-      uint tokenId = getTokenId(adminSig);
-      minted[tokenId][_msgSender()] = true;
-      _mint(_msgSender(), tokenId, 1, data);
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
     }
 
-  function getTokenId(bytes calldata adminSig) public pure returns (uint) {
-      // decode bytes and parse out tokenId from signature
-      return 1;
-  }
+    function mint(
+        address account,
+        uint256 id,
+    ) public {
+        require(msg.sender == minter);
+        _mint(account, id, 1, ""); // mints 1 NFT of chosen ID
+    }
 
-  function _isValid(bytes calldata adminSig) internal pure returns (bool) {
-      // use ecrecover to get address of signer
-      // check signer address == owner/admin address of contract
-      // check msgSender == minter address in signed message
-      // check block.timestamp <= timestamp + 100 in signed message
-      // (timestamp necessary to prevent reuse of signature)
-      return true;
-  }
+    function updateMinter(address _minter) onlyOwner {
+      minter = _minter;
+    }
+
+    function updateBaseURI(string memory _based) onlyOwner{
+        _setBaseURI(based);
+    }
+
+    function uri(uint256 tokenId) public view virtual override returns (string memory) {
+      string memory tokenURI = _tokenURIs[tokenId];
+
+      // If token URI is set, concatenate base URI and tokenURI (via abi.encodePacked).
+      //Note to dev: upload a folder to IPFS that has all the jsons in it numbered and named 1.json for example.
+      return string(abi.encodePacked(_baseURI, tokenURI, ".json"));
+    }
+
 }
