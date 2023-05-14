92235d5dd3aa055ca9473723940c2a9383c5efbc
Konrad Gnat <konradmgnat@gmail.com>
2023-05-14 16:10:10 +0800

    update contract to limitminting to one

--- contracts/PuzzleNFT.sol
+++ contracts/PuzzleNFT.sol
@@ -6,16 +6,16 @@ import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
 
 /// @custom:security-contact team@usekeyp.com
 contract PuzzleNFT is Ownable, ERC1155URIStorage {
-    struct UserHasMinted {
+    struct UserExists {
       bool isValue;
     }
 
-    mapping(address => bool) isMinter;
-    mapping(uint256 => mapping(address => UserHasMinted)) public tokenIdToOwner;
+    mapping(address => UserExists) isMinter;
+    mapping(uint256 => mapping(address => UserExists)) public tokenIdToOwner;
     string public baseURI;
 
     constructor(string memory _baseURI) ERC1155(_baseURI) {
-        isMinter[msg.sender] = true;
+        isMinter[msg.sender] = UserExists(true);
         baseURI = _baseURI;
     }
 
@@ -24,18 +24,21 @@ contract PuzzleNFT is Ownable, ERC1155URIStorage {
     }
 
     function mint(address account, uint256 id) public {
-        require(isMinter[msg.sender], "Only minter address is allowed to mint");
-        require(tokenIdToOwner[id][account].isValue != true, "Maximum 1 token per address");
+        require(isMinter[msg.sender].isValue == true, "Only minter address is allowed to mint");
+        // only non minter addresses must not mint more than 1 token
+        if (isMinter[account].isValue != true) {
+          require(tokenIdToOwner[id][account].isValue != true, "Maximum 1 token per address");
+        }
         _mint(account, id, 1, ""); // mints 1 NFT of chosen ID
-        tokenIdToOwner[id][account] = UserHasMinted(true);
+        tokenIdToOwner[id][account] = UserExists(true);
     }
 
     function addMinter(address _minter) public onlyOwner {
-        isMinter[_minter] = true;
+        isMinter[_minter] = UserExists(true);
     }
 
     function removeMinter(address _minter) public onlyOwner {
-        isMinter[_minter] = false;
+        isMinter[_minter] = UserExists(false);
     }
 
     function updateBaseURI(string memory _based) public onlyOwner {
